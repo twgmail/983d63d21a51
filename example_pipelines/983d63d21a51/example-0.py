c1019d5c5425e79f6aa0bf3186bf664ae4ba906a
TheodorWienert <theo@wienert-hamburg.de>
2025-06-30 14:12:48 +0200

    Init2

---
+++ example_pipelines/983d63d21a51/example-0.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
+
+# Setting up paths
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from utils import get_project_root
+
+# Getting the project root
+project_root = get_project_root()
+
+# Getting the raw data file
+raw_data_file = os.path.join(
+    project_root, "datasets", "8c2a25260209", "8c2a25260209.csv"
+)
+data = pd.read_csv(raw_data_file)
+
+X = data.drop(["Survived", "Name", "Ticket", "Cabin", "PassengerId"], axis=1)
+y = data["Survived"]
+
+
+X = pd.get_dummies(X, columns=["Sex", "Embarked"])
+X["Age"] = X["Age"].fillna(X["Age"].mean())
+
+
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y_encoded, test_size=0.2, random_state=42
+)
+
+# Training model
+clf = RandomForestClassifier(random_state=42)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+# Evaluating model
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification report:", classification_report(y_test, y_pred))
